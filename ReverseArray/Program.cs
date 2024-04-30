aa7f4a3395dfbda62f9b55f5b8ecdf3a915c4dcb
olga-vorontsovskaya <o.vorontsovskaya@gmail.com>
2024-04-30 17:27:39 +0300

    Add project files.

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584 };
+
+            for (int i = arr.Length - 1; i >= 0;  i--) // arr.Length minus 1 leads to the last element of array
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
